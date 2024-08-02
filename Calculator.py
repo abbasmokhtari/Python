c5d8d213c09159421e82fba429fcb8cb2e3a2e5e
Abbas Mokhtari <105991104+Mahan-Mokhtari@users.noreply.github.com>
2024-08-02 23:09:00 +0100

    First commit on first project

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+op = input("Please choose your operator: +, -, / and * ")
+
+def cal(op):
+    a= int(input("give a:"))
+    b = int(input("give b:"))
+    if op == "+":
+        return a + b
+    elif op == "-":
+        return a - b
+    elif op == "/":
+        return a / b
+    elif op == "*":
+        return a * b
+c = op.strip() #We used strip to get rid of spaces in input
+if c == "+" or c == "-" or c == "/" or c == "*" :
+    print(cal(c))
+else: 
+    print("Please try again")
