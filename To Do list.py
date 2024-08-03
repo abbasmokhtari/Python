ff304fca1e7a46d7cbe23221596ea8b8b93f8114
Abbas Mokhtari <abs.mokhtari@gmail.com>
2024-08-03 23:22:22 +0100

    todo list session one
    external file was opened and data was added

--- To Do list.py
+++ To Do list.py
@@ -1,1 +1,23 @@
-# todo list making ext management locla txt viniall list amnd file imports
+data = open('Data.txt', 'r') #external file was opened and read permission given 
+items = data.read().split()        #external file was read and turned into array
+item = ''
+
+while item.lower() != 'q':
+    item = input('Add item to your list')
+    items.append(item)
+    if item.lower() == 'q':
+        items.pop(-1) #-1 is thee last item in our array
+        # this is how we open a txt file
+        f = open('Data.txt', 'w') # a is command for append
+
+        for i in range(len(items)):
+            f.write(str(items[i]) + ' ') # we added space to make sure words won't stick together
+    
+        f.close()
+        print(items)
+        break
+
+
+#remove items in next session with Mahan
+
+
