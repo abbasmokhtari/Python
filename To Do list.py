b1034557e5c46e07d0518c67f9b733a4230cfee1
Abbas Mokhtari <abs.mokhtari@gmail.com>
2024-08-04 22:46:53 +0100

    Remove option added
    user can type item number to remove form list and try/except method was used to differentiate between number and words

--- To Do list.py
+++ To Do list.py
@@ -2,20 +2,36 @@ data = open('Data.txt', 'r') #external file was opened and read permission given
 items = data.read().split()        #external file was read and turned into array
 item = ''
 
-while item.lower() != 'q':
-    item = input('Add item to your list')
-    items.append(item)
-    if item.lower() == 'q':
-        items.pop(-1) #-1 is thee last item in our array
-        # this is how we open a txt file
-        f = open('Data.txt', 'w') # a is command for append
 
-        for i in range(len(items)):
-            f.write(str(items[i]) + ' ') # we added space to make sure words won't stick together
+print('This is your To do List ', items)
+print('Please type item number to remove or start adding new items')
+
+
+while item.lower() != 'q':
+    item = input('Add or remove item to your list')
     
-        f.close()
+    try:    
+       items.pop(int(item) - 1)     
+       print(items) 
+    except ValueError:
+        items.append(item)
+      
+        if item.lower() == 'q':
+
+            items.pop(-1) #-1 is the last item in our array we wanted to remove q
+            # this is how we open a txt file
+            f = open('Data.txt', 'w') # a is command for append
+
+            for i in range(len(items)):
+                f.write(str(items[i]) + ' ') # we added space to make sure words won't stick together
+            f.close()
+            break
+    
+            
         print(items)
-        break
+   
+        
+           
 
 
 #remove items in next session with Mahan
