b76b336cd36e106ee032bd6cc21d3d4febe68baf
Abbas Mokhtari <abs.mokhtari@gmail.com>
2024-08-06 19:35:14 +0100

    last commit JSON
    convert Json to dict and vise versa

--- python_sandbox-master/python_sandbox_starter/py_json.py
+++ python_sandbox-master/python_sandbox_starter/py_json.py
@@ -1,1 +1,26 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+
+import json
+
+# Sample JSON
+userJSON = '{"first_name": "Abbas", "last_name": "Mokhtari", "age": 44}'
+
+# Parse JSON to Dictionary 
+user = json.loads(userJSON)
+
+print(user)
+print(user['first_name'])
+
+
+# Turn a dict to JSON
+
+car = {
+    'make': 'Mercedes',
+    'model': 'E200',
+    'year': 2020
+}
+
+carJSON = json.dumps(car)
+
+print(carJSON)
