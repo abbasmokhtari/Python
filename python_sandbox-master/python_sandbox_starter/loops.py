4f52f46a62a80afe1a88789f4a4bba14672d4161
Abbas Mokhtari <abs.mokhtari@gmail.com>
2024-08-06 12:27:27 +0100

    for and while loops over
    for while break continue and range is covered

--- python_sandbox-master/python_sandbox_starter/loops.py
+++ python_sandbox-master/python_sandbox_starter/loops.py
@@ -1,5 +1,36 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John', 'Gavin', 'Sara', 'Fresia']
 
+# Simple for loop
+for person in people:
+    print(f'Current person: {person}')
+
+# Break in this example we stopped as soon as we hit Sara
+for person in people:
+    if person == 'Sara':
+        break
+    print(f'current person: {person}')
+
+# contniue  in this example we just didn't print Sara
+for person in people:
+    if person == 'Sara':
+        continue
+    print(f'Current person: {person}')
+
+# range
+for i in range(len(people)):
+    print(people[i])
+
+
+for i in range(1, 8):   # in this example it won't count the last number in range
+    print(f'Number is: {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+
+
+count = 0
+while count <= 5:
+    print(f'Count: {count}')
+    count += 1
+    
